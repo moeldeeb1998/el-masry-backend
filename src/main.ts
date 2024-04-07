b283fd04916eb4e3ee7fe6ed672ae97f036dc707
Mohamed Eldeeb <meldeeb1998@gmail.com>
2024-04-07 23:17:30 +0200

    add validation pip for DTOs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggerService } from './logger/logger.service';
+import { ValidationPipe } from '@nestjs/common';
 
 const customeLogger = new LoggerService();
 
@@ -8,6 +9,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: customeLogger,
   });
+  app.useGlobalPipes(
+    // validate DTOs
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(process.env.APP_PORT || 3000, () => {
     customeLogger.info(
       `App started at ${process.env.APP_HOST}:${process.env.APP_PORT}`,
